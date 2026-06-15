e6fa5f6a11ac3bac12cc0787b48223da2972d31b
Shiva Singh Shishodia <130183426+shivasinghshishodia@users.noreply.github.com>
2026-06-15 15:16:40 +0530

    Add files via upload

---
+++ java/Activity5.java
@@ -0,0 +1,27 @@
+package fst_java;
+
+abstract class Book {
+	String title;
+
+	abstract void setTitle(String title);
+
+	String getTitle() {
+		return title;
+	}
+}
+
+class MyBook extends Book {
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
+
+public class Activity5 {
+	public static void main(String[] args) {
+		String title = "Hover Car Racer";
+		Book newNovel = new MyBook();
+		newNovel.setTitle(title);
+
+		System.out.println("The title is: " + newNovel.getTitle());
+	}
+}
